a5a16efa1ff604486c9e61a5293e587f0a65aaa3
Gideon Bakx <gideon@bakx.ca>
2017-05-22 20:59:49 -0400

    Removed the ModernUI project. Initiated work to replace the validation adorner with a pop-up.

---
+++ 1.0/ModernUI/ModernUI.WPF4/Behaviors/RepositionPopupBehavior.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Interactivity;
+
+namespace ModernUI.Behaviors
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class RepositionPopupBehavior : Behavior<Popup>
+    {
+        /// <summary>
+        /// Called after the behavior is attached to an <see cref="Behavior.AssociatedObject"/>.
+        /// </summary>
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            Window window = Window.GetWindow(AssociatedObject.PlacementTarget);
+            if (window == null) { return; }
+            window.LocationChanged += OnLocationChanged;
+            window.SizeChanged += OnSizeChanged;
+            AssociatedObject.Loaded += AssociatedObject_Loaded;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        static void AssociatedObject_Loaded(object sender, RoutedEventArgs e)
+        {
+            //AssociatedObject.HorizontalOffset = 7;
+            //AssociatedObject.VerticalOffset = -AssociatedObject.Height;
+        }
+
+        /// <summary>
+        /// Called when the behavior is being detached from its <see cref="Behavior.AssociatedObject"/>, but before it has actually occurred.
+        /// </summary>
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            Window window = Window.GetWindow(AssociatedObject.PlacementTarget);
+            if (window == null) { return; }
+            window.LocationChanged -= OnLocationChanged;
+            window.SizeChanged -= OnSizeChanged;
+            AssociatedObject.Loaded -= AssociatedObject_Loaded;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnLocationChanged(object sender, EventArgs e)
+        {
+            double offset = AssociatedObject.HorizontalOffset;
+            AssociatedObject.HorizontalOffset = offset + 1;
+            AssociatedObject.HorizontalOffset = offset;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            double offset = AssociatedObject.HorizontalOffset;
+            AssociatedObject.HorizontalOffset = offset + 1;
+            AssociatedObject.HorizontalOffset = offset;
+        }
+    }
+}
